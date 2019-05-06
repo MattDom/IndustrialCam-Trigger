533e402420c221baa7108418982dc76ccab37fbc
Matthias Domnik <matthias.domnik@googlemail.com>
2019-05-06 22:16:33 +0200

    Changed option for offsetting the leds

--- camera_trigger/camera_trigger/include/trigger_modes.h
+++ camera_trigger/camera_trigger/include/trigger_modes.h
@@ -3,7 +3,7 @@
  *
  * Created: 28.04.2019 16:06:02
  *  Author: Matthias
- */ 
+ */
 
 #ifndef TRIGGER_MODES_H_
 #define TRIGGER_MODES_H_
@@ -12,7 +12,7 @@ const int8_t low_base_ = 10;
 const int8_t med_base_ = 0;
 const int8_t high_base_ = 0;
 
-const uint8_t number_leds = 3;
+const uint8_t number_leds = 4;
 
 int8_t low_temp_ = 0;
 int8_t med_temp_ = 0;
@@ -26,187 +26,124 @@ uint8_t time_delay_0 = 0;
 uint8_t loop_mode_select = 0;
 uint8_t loop_count = 0;
 uint8_t num_leds = 0;
+uint8_t time_ms_ = 0;
 
-void low_freq()
-{		  
-	if (num_leds < number_leds)
-	{
-	if (time_delay_0 == low_value)
-	{
-		PORTB |= (1 << DDB4);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 10)
-	{
-		PORTB &= ~(1 << DDB4);
-		PORTB |= (1 << DDB0);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 20)
-	{
-		PORTB &= ~(1 << DDB0);
-		PORTB |= (1 << DDB2);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 30)
-	{
-		PORTB &= ~(1 << DDB2);
-		PORTD |= (1 << DDD7);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 40)
-	{
-		PORTD &= ~(1 << DDD7);
-		PORTB |= (1 << DDB3);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 50)
-	{
-		PORTB &= ~(1 << DDB3);
-		PORTD |= (1 << DDD5);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 60)
-	{
-		PORTD &= ~(1 << DDD5);
-		PORTD |= (1 << DDD4);
-		num_leds++;
-	}
-	else if (time_delay_0 == low_value + 70)
-	{
-		PORTD &= ~(1 << DDD4);
-	}
-	}
+void low_freq() {
+  PORTD &= ~(0xBF);
+  PORTB &= ~(0x0F);
+
+  time_ms_ = time_delay_0 + low_value;
+
+  if (num_leds < number_leds) {
+    if (time_ms_ == 0) {
+      PORTB |= (1 << DDB4);
+      num_leds++;
+    } else if (time_ms_ == 10) {
+      PORTB |= (1 << DDB0);
+      num_leds++;
+    } else if (time_ms_ == 20) {
+      PORTB |= (1 << DDB2);
+      num_leds++;
+    } else if (time_ms_ == 30) {
+      PORTD |= (1 << DDD7);
+      num_leds++;
+    } else if (time_ms_ == 40) {
+      PORTB |= (1 << DDB3);
+      num_leds++;
+    } else if (time_ms_ == 50) {
+      PORTD |= (1 << DDD5);
+      num_leds++;
+    } else if (time_ms_ == 60) {
+      PORTD |= (1 << DDD4);
+      num_leds++;
+    }
+  }
 }
 
-void med_freq()
-{		  
-	if (num_leds < number_leds)
-	{	
-	if (time_delay_0 == med_value)
-	{
-		PORTB |= (1 << DDB4);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 5)
-	{
-		PORTB &= ~(1 << DDB4);
-		PORTB |= (1 << DDB0);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 10)
-	{
-		PORTB &= ~(1 << DDB0);
-		PORTB |= (1 << DDB2);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 15)
-	{
-		PORTB &= ~(1 << DDB2);
-		PORTD |= (1 << DDD7);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 20)
-	{
-		PORTD &= ~(1 << DDD7);
-		PORTB |= (1 << DDB3);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 25)
-	{
-		PORTB &= ~(1 << DDB3);
-		PORTD |= (1 << DDD5);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 35)
-	{
-		PORTD &= ~(1 << DDD5);
-		PORTD |= (1 << DDD4);
-		num_leds++;
-	}
-	else if (time_delay_0 == med_value + 40)
-	{
-		PORTD &= ~(1 << DDD4);
-	}    
-	}
+void med_freq() {
+  PORTD &= ~(0xBF);
+  PORTB &= ~(0x0F);
+
+  time_ms_ = time_delay_0 + med_value;
+
+  if (num_leds < number_leds) {
+    if (time_ms_ == 0) {
+      PORTB |= (1 << DDB4);
+      num_leds++;
+    } else if (time_ms_ == 5) {
+      PORTB |= (1 << DDB0);
+      num_leds++;
+    } else if (time_ms_ == 10) {
+      PORTB |= (1 << DDB2);
+      num_leds++;
+    } else if (time_ms_ == 15) {
+      PORTD |= (1 << DDD7);
+      num_leds++;
+    } else if (time_ms_ == 20) {
+      PORTB |= (1 << DDB3);
+      num_leds++;
+    } else if (time_ms_ == 25) {
+      PORTD |= (1 << DDD5);
+      num_leds++;
+    } else if (time_ms_ == 35) {
+      PORTD |= (1 << DDD4);
+      num_leds++;
+    }
+  }
 }
 
-void high_freq()
-{			  
-	if (num_leds < number_leds)
-	{
-	if (time_delay_0 == high_value)
-	{
-		PORTB |= (1 << DDB4);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 1)
-	{
-		PORTB &= ~(1 << DDB4);
-		PORTB |= (1 << DDB0);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 2)
-	{
-		PORTB &= ~(1 << DDB0);
-		PORTB |= (1 << DDB2);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 3)
-	{
-		PORTB &= ~(1 << DDB2);
-		PORTD |= (1 << DDD7);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 4)
-	{
-		PORTD &= ~(1 << DDD7);
-		PORTB |= (1 << DDB3);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 5)
-	{
-		PORTB &= ~(1 << DDB3);
-		PORTD |= (1 << DDD5);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 6)
-	{
-		PORTD &= ~(1 << DDD5);
-		PORTD |= (1 << DDD4);
-		num_leds++;
-	}
-	else if (time_delay_0 == high_value + 7)
-	{
-		PORTD &= ~(1 << DDD4);
-	}	
-	}
+void high_freq() {
+  PORTD &= ~(0xBF);
+  PORTB &= ~(0x0F);
+
+  time_ms_ = time_delay_0 + high_value;
+
+  if (num_leds < number_leds) {
+    if (time_ms_ == high_value) {
+      PORTB |= (1 << DDB4);
+      num_leds++;
+    } else if (time_ms_ == 1) {
+      PORTB |= (1 << DDB0);
+      num_leds++;
+    } else if (time_ms_ == 2) {
+      PORTB |= (1 << DDB2);
+      num_leds++;
+    } else if (time_ms_ == 3) {
+      PORTD |= (1 << DDD7);
+      num_leds++;
+    } else if (time_ms_ == 4) {
+      PORTB |= (1 << DDB3);
+      num_leds++;
+    } else if (time_ms_ == 5) {
+      PORTD |= (1 << DDD5);
+      num_leds++;
+    } else if (time_ms_ == 6) {
+      PORTD |= (1 << DDD4);
+      num_leds++;
+    }
+  }
 }
 
-void loop_mode()
-{
-	switch(loop_mode_select % 3)
-	{
-		case 0: 		
-		  low_freq();
-		  loop_count++;
-		  break;
-		case 1:
-     	  med_freq();
-		  loop_count++;
-		  break;
-		case 2:	
-		  high_freq();
-		  loop_count++;	  
-		  break;
-	}
+void loop_mode() {
+  switch (loop_mode_select % 3) {
+    case 0:
+      low_freq();
+      loop_count++;
+      break;
+    case 1:
+      med_freq();
+      loop_count++;
+      break;
+    case 2:
+      high_freq();
+      loop_count++;
+      break;
+  }
 
-	if (loop_count == 100)
-	{
-		loop_count = 0;
-		loop_mode_select++;
-	}
-	
+  if (loop_count == 100) {
+    loop_count = 0;
+    loop_mode_select++;
+  }
 }
 
 #endif /* TRIGGER_MODES_H_ */
--- camera_trigger/camera_trigger/main.c
+++ camera_trigger/camera_trigger/main.c
@@ -1,9 +1,9 @@
 /*
- * IndustrialCam-trigger
- *
- * Created: 13.01.2019 10:00:23
- * Author : Matthias
- */
+* IndustrialCam-trigger
+*
+* Created: 13.01.2019 10:00:23
+* Author : Matthias Domnik
+*/
 
 #define F_CPU 16000000UL
 
@@ -26,206 +26,206 @@ bool init_shot_ = true;
 void single_shot();
 
 int main(void) {
-  DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4);
-  DDRC = 0x00;
-  PORTC = 0xFF;	  
-  DDRD |= (1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
-  DDRD &= ~(1 << DDD2) & ~(1 << DDD3);
+    DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4);
+    DDRC = 0x00;
+    PORTC = 0xFF;
+    DDRD |= (1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
+    DDRD &= ~(1 << DDD2) & ~(1 << DDD3);
 
-  // Timer 1 konfigurieren
-  TCCR1A |= (1 << COM1A1);  // Toggle OC1A/OC1B on Compare Match
-  TCCR1B &= ~(1 << CS12) & ~(1 << CS11) & ~(1 << CS10);
-  TCCR1B |= (1 << WGM12);  // CTC-Mode
-  OCR1A = 249;             // Plus eins für den ÜberlauF!
+    // Timer 1 konfigurieren
+    TCCR1A |= (1 << COM1A1);  // Toggle OC1A/OC1B on Compare Match
+    TCCR1B &= ~(1 << CS12) & ~(1 << CS11) & ~(1 << CS10);
+    TCCR1B |= (1 << WGM12);  // CTC-Mode
+    OCR1A = 249;             // Plus eins für den ÜberlauF!
 
-  // Compare Interrupt erlauben
-  TIMSK1 |= (1 << OCIE1A);
+    // Compare Interrupt erlauben
+    TIMSK1 |= (1 << OCIE1A);
 
-  EICRA |= ((1 << ISC01) | (1 << ISC00));  // Enable INT0
+    EICRA |= ((1 << ISC01) | (1 << ISC00));  // Enable INT0
 
-  // Global Interrupts aktivieren
-  sei();
+    // Global Interrupts aktivieren
+    sei();
 
-  TCNT1H = 0x0;
-  TCNT1L = 0x0;
-  TCCR1B |= (1 << CS11) | (1 << CS10);  // Timer 1 aktivieren (Prescaler auf 64)
+    TCNT1H = 0x0;
+    TCNT1L = 0x0;
+    TCCR1B |= (1 << CS11) | (1 << CS10);  // Timer 1 aktivieren (Prescaler auf 64)
 
-  while (1) {
-    if (PIND & (1 << DDD3))  // Check for single-shot-mode
-    {
-      if (button) {
-        if (shot_done) {
-          TCCR1B &= ~(1 << CS12) & ~(1 << CS11) & ~(1 << CS10);
-          button = false;
-          shot_done = false;
-          TCNT1H = 0x0;
-          TCNT1L = 0x0;
-          time_delay_0 = 0;
-          PORTD &= 0x4F;
-          PORTB &= 0x1F;
-        }
-      } else if (init_shot_) {
-        TCCR1B &= ~(1 << CS12) & ~(1 << CS11) & ~(1 << CS10);
-        PORTD |= (1 << DDD6);
-        EIMSK |= (1 << INT0);
-        PORTD &= 0x4F;
-        PORTB &= 0x1F;
-        init_shot_ = false;
-      }
-    } else {
-      EIMSK &= ~(1 << INT0);                // Disable EXT0
-      TCCR1B |= (1 << CS11) | (1 << CS10);  // Enable Timer1
-      init_shot_ = true;                    // Reset flag
+    while (1) {
+        if (PIND & (1 << DDD3))  // Check for single-shot-mode
+        {
+            if (button) {
+                if (shot_done) {
+                    TCCR1B &= ~(1 << CS12) & ~(1 << CS11) & ~(1 << CS10);
+                    button = false;
+                    shot_done = false;
+                    TCNT1H = 0x0;
+                    TCNT1L = 0x0;
+                    time_delay_0 = 0;
+                    PORTD &= 0x4F;
+                    PORTB &= 0x1F;
+                }
+                } else if (init_shot_) {
+                TCCR1B &= ~(1 << CS12) & ~(1 << CS11) & ~(1 << CS10);
+                PORTD |= (1 << DDD6);
+                EIMSK |= (1 << INT0);
+                PORTD &= 0x4F;
+                PORTB &= 0x1F;
+                init_shot_ = false;
+            }
+            } else {
+            EIMSK &= ~(1 << INT0);                // Disable EXT0
+            TCCR1B |= (1 << CS11) | (1 << CS10);  // Enable Timer1
+            init_shot_ = true;                    // Reset flag
 
-      // PC0 - PC1
-	  pin_mode_ = 0;
-      pin_mode_ |= (PINC & (1 << DDC1));
-      pin_mode_ |= (PINC & (1 << DDC0));
+            // PC0 - PC1
+            pin_mode_ = 0;
+            pin_mode_ |= (PINC & (1 << DDC1));
+            pin_mode_ |= (PINC & (1 << DDC0));
 
-      // PC3 - PC5
-	  offset_ = 0;
-      offset_ |= ((PINC & (1 << DDC5))>>2) | ((PINC & (1 << DDC4))>>2) | ((PINC & (1 << DDC3))>>2) | ((PINC & (1 << DDC2))>>2);
+            // PC3 - PC5
+            offset_ = 0;
+            offset_ |= ((PINC & (1 << DDC5))>>2) | ((PINC & (1 << DDC4))>>2) | ((PINC & (1 << DDC3))>>2) | ((PINC & (1 << DDC2))>>2);
 
-      // Switch to selcted mode
-      switch (pin_mode_) {
-        case 0:
-          mode = loop_;
-          break;
-        case 1:
-          mode = low_;
-          switch (offset_) {
-            case 0:
-              low_temp_ = -10;
-              break;
-            case 1:
-              low_temp_ = 0;
-              break;
-            case 2:
-			  low_temp_ = 10;
-              break;
-            case 3:
-              low_temp_ = 20;
-              break;
-            case 4:
-              low_temp_ = 30;
-              break;
-			case 5:
-              low_temp_ = 40;
-              break;
-			case 6:
-              low_temp_ = 50;
-              break;
-			case 7:
-              low_temp_ = 60;
-              break;
-			case 8:
-              low_temp_ = 70;
-              break;			  			  
-            default:
-			  low_temp_ = 0;
-              break;
-          }
+            // Switch to selected mode
+            switch (pin_mode_) {
+                case 0:
+                mode = loop_;
+                break;
+                case 1:
+                mode = low_;
+                switch (offset_) {
+                    case 0:
+                    low_temp_ = -10;
+                    break;
+                    case 1:
+                    low_temp_ = 0;
+                    break;
+                    case 2:
+                    low_temp_ = 10;
+                    break;
+                    case 3:
+                    low_temp_ = 20;
+                    break;
+                    case 4:
+                    low_temp_ = 30;
+                    break;
+                    case 5:
+                    low_temp_ = 40;
+                    break;
+                    case 6:
+                    low_temp_ = 50;
+                    break;
+                    case 7:
+                    low_temp_ = 60;
+                    break;
+                    case 8:
+                    low_temp_ = 70;
+                    break;
+                    default:
+                    low_temp_ = 0;
+                    break;
+                }
+                low_value = low_base_ + low_temp_;
+                break;
+                
+                case 3:
+                mode = medium_;
+                med_value += low_value;
+                switch (offset_) {
+                    case 0:
+                    med_temp_ = 0;
+                    break;
+                    case 1:
+                    med_temp_ = 2;
+                    break;
+                    case 2:
+                    med_temp_ = 4;
+                    break;
+                    case 3:
+                    med_temp_ = 6;
+                    break;
+                    case 4:
+                    med_temp_ = 8;
+                    break;
+                    default:
+                    med_temp_ = 0;
+                    break;
+                }
 
-          low_value = low_base_ + low_temp_;
-          break;
-        case 3:
-          mode = medium_;
-          med_value += low_value;
-          switch (offset_) {
-            case 0:
-              med_temp_ = 0;
-              break;
-            case 1:
-              med_temp_ = 2;
-              break;
-            case 2:
-			  med_temp_ = 4;
-              break;
-            case 3:
-              med_temp_ = 6;
-              break;
-            case 4:
-              med_temp_ = 8;
-              break;
-            default:
-			  med_temp_ = 0;
-              break;
-          }
-
-          med_value = med_base_ + med_temp_ + low_value;
-          break;
-        case 2:
-          mode = high_;
-          high_value += med_value;
-          switch (offset_) {
-            case 0:
-			  high_temp_ = 0;              
-              break;
-            case 1:
-              high_temp_ = -2;
-              break;
-            case 2:
-			  high_temp_ = -1;
-              break;
-            case 3:
-              high_temp_ = 1;
-              break;
-            case 4:
-              high_temp_ = 2;
-              break;
-            default:
-			  high_temp_ = 0;
-              break;
-          }
-
-          high_value = high_base_ + high_temp_ + med_value;
-          break;
-        default:
-          break;
-      }  // switch-case
-    }    // if...else
-  }      // while()
+                med_value = med_base_ + med_temp_ + low_value;
+                break;
+                case 2:
+                mode = high_;
+                high_value += med_value;
+                switch (offset_) {
+                    case 0:
+                    high_temp_ = 0;
+                    break;
+                    case 1:
+                    high_temp_ = -2;
+                    break;
+                    case 2:
+                    high_temp_ = -1;
+                    break;
+                    case 3:
+                    high_temp_ = 1;
+                    break;
+                    case 4:
+                    high_temp_ = 2;
+                    break;
+                    default:
+                    high_temp_ = 0;
+                    break;
+                }
+                high_value = high_base_ + high_temp_ + med_value;
+                break;
+                
+                default:
+                break;
+            }  // switch-case
+        }    // if...else
+    }      // while()
 }  // main()
 
 ISR(TIMER1_COMPA_vect)
 {
-  if (mode == loop_) {
-    loop_mode();
-  } else if (mode == low_) {
-    low_freq();
-  } else if (mode == medium_) {
-    med_freq();
-  } else if (mode == high_) {
-    high_freq();
-  }
-  time_delay_0++;
-  // ####### 100ms Periode #######
-  if (time_delay_0 <= 10) {
-    PORTD &= ~(1 << DDD6);
-  } else {
-    PORTD |= (1 << DDD6);  // Fire trigger
-    if (time_delay_0 == 100) {
-      time_delay_0 = 0;
-      shot_done = true;
-	  num_leds = 0;
+    if (mode == loop_) {
+        loop_mode();
+        } else if (mode == low_) {
+        low_freq();
+        } else if (mode == medium_) {
+        med_freq();
+        } else if (mode == high_) {
+        high_freq();
     }
-  }  
+    time_delay_0++;
+    // ####### 100ms Periode #######
+    if (time_delay_0 <= 10) {
+        PORTD &= ~(1 << DDD6);
+        } else {
+        PORTD |= (1 << DDD6);  // Fire trigger
+        if (time_delay_0 == 100) {
+            time_delay_0 = 0;
+            shot_done = true;
+            num_leds = 0;
+        }
+    }
 }
 
 ISR(INT0_vect) {
-  while (PIND & (1 << DDD2)) {
-    _delay_ms(1);
-  };
-  if (!button) {
-    button = true;
-    single_shot();
-  }
+    while (PIND & (1 << DDD2)) {
+        _delay_ms(1);
+    };
+    if (!button) {
+        button = true;
+        single_shot();
+    }
 }
 
 void single_shot() {
-  shot_done = false;
-  TCNT1H = 0x0;
-  TCNT1L = 0x0;
-  time_delay_0 = 0;
-  TCCR1B |= (1 << CS11) | (1 << CS10);
+    shot_done = false;
+    TCNT1H = 0x0;
+    TCNT1L = 0x0;
+    time_delay_0 = 0;
+    TCCR1B |= (1 << CS11) | (1 << CS10);
 }
